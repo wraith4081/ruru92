fa42ef5387059d91aeafed7c651d43ea260abca8
wraith4081 <54374743+wraith4081@users.noreply.github.com>
2023-04-01 13:56:25 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+};
+
+navigator.serviceWorker.addEventListener("install", (event) => {
+    // Request to React's assets
+    fetch('/asset-manifest.json').then(r => r.json()).then((assets) => {
+        const entrypoints = JSON.parse(assets).entrypoints;
+
+        event.waitUntil(
+            addResourcesToCache([
+                "/",
+                "/index.html",
+                ...entrypoints,
+                "/logo-white.png",
+                "/logo64.png",
+                "/logo192.png",
+                "/512.png",
+                "/manifest.json",
+                "/robots.txt",
+                "/asset-manifest.json",
+            ])
+        );
+    });
+});
